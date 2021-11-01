d697469a9112876fc9e8fb727b30be023f65ad26
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-11-01 13:48:53 +0100

    Create pi.cpp

---
+++ pi.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //Input-Output Programme (schon vorhanden)
+#include <cmath>    //Mathe funktionen
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+    double x = 0.0;
+    double y = 0.0;
+    int i = 0;
+    double innerring = 0;
+
+    while(i < 100000000) {
+            x = (1.0 * rand())/RAND_MAX; 
+            y = (1.0 * rand())/RAND_MAX; 
+            i += 1;
+            
+            if (x*x+y*y <= 1)
+                innerring += 1;
+            
+    }       
+        std::cout << innerring << "\n\r";
+        double pi = (innerring/100000000)*4;
+        std::cout << pi;
+
+    }
